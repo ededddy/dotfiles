b6ec6164a11f763fbd6dbdb6aea8f593613135cf
Eddy Lei <43884159+ededddy@users.noreply.github.com>
2021-09-23 01:18:13 +0800

    added pwsh profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+Set-PSReadLineOption -BellStyle Audible -DingTone 1221 -DingDuration 60
+Set-PoshPrompt Zash
+neofetch
