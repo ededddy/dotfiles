efcf5fc98aebd72211a2bb33cae004357c8e27fe
Eddy Lei <eddylei070300@gmail.com>
2021-11-03 11:00:41 +0800

    delete stuff

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Import-Module -Name Terminal-Icons
-Set-PSReadLineOption -BellStyle Audible -DingTone 1221 -DingDuration 60
-Set-PoshPrompt Zash
-neofetch
